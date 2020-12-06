72067ad8cadcec1fec6d2d84aab802e7ea505024
Maksym Malynovskyi <maksym.1002@gmial.com>
2020-12-06 14:33:31 +0000

    added the functionality and style

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React, {useState, useEffect} from 'react';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import Message from './Message'
 import './App.css';
+import db from './firebase'
+import firebase from 'firebase'
+import FlipMove from 'react-flip-move'
+import SendIcon from '@material-ui/icons/Send';
+import { IconButton } from '@material-ui/core';
 
 
 function App() {
 const [input, setInput]= useState('');
-const [messages, setMessages]= useState([{username: 'Malyrion', text: 'this is test message'},{username: 'Random user', text: 'hi'}]);
+const [messages, setMessages]= useState([]);
 const [username, setUsername]= useState('');
 
 //useState = variable in React
 //useEffect = run code on a condition in React
+
+useEffect(()=>{
+  //run once when the app component is loads
+    db.collection('messages').orderBy('timestamp', 'desc').onSnapshot(snapshot =>{
+      setMessages(snapshot.docs.map(doc => ({id: doc.id, message: doc.data()})))
+    });
+},[])
 
 useEffect (()=>{
   //run code here on a condition
@@ -22,28 +34,39 @@ useEffect (()=>{
 const sendMessage=(event)=>{
   //logic for send messages
   event.preventDefault();
-  setMessages([...messages,{username: username, text: input}]);
+  // setMessages([...messages,{username: username, message: input}]);
+  db.collection('messages').add({
+    username: username,
+    message: input,
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+
+    })
   setInput('')
 }
   return (
     <div class="App">
+        <img src='https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100'/>
         <h1>Messenger</h1>
         <h2>Wellcome {username}</h2>
-        <form>
-        <FormControl>
-          <InputLabel htmlFor="my-input">Enter a message</InputLabel>
-          <Input value={input} onChange={event => setInput(event.target.value)}/>
-          <Button disabled={!input}type='submit'onClick={sendMessage} variant="contained" color="primary">Send Message</Button>
+        <form className="app__form">
+        <FormControl className="app__formControl">
+          <Input className="app__input" placeholder="Enter a message..." value={input} onChange={event => setInput(event.target.value)}/>
+          <IconButton className="app__iconButton" disabled={!input}type='submit'onClick={sendMessage} variant="contained" color="primary">
+          <SendIcon/>
+          </IconButton>
         </FormControl>
         </form>
 
       {/* messages themselves*/}
+      <FlipMove>
       {
-        messages.map(message => (
-          <Message username={username} message={message}/>
+        messages.map(({id,message}) => (
+          <Message key={id} username={username} message={message}/>
           // <p>{message}</p>
         ))
       }
+      </FlipMove>
+
       {/**/}
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 
 
-const firebaseApp = firebase.initializeApp {
+const firebaseApp = firebase.initializeApp ({
   apiKey: "AIzaSyDJBsysnlFJ0WSwVkwr4w4PlnS-36ICeOw",
   authDomain: "messenger-ca304.firebaseapp.com",
   projectId: "messenger-ca304",
@@ -9,7 +9,7 @@ const firebaseApp = firebase.initializeApp {
   messagingSenderId: "306696602887",
   appId: "1:306696602887:web:3d888b6a81171359d1b1a2",
   measurementId: "G-CBMEWBLY86"
-};
+});
 
 const db = firebaseApp.firestore();
 
